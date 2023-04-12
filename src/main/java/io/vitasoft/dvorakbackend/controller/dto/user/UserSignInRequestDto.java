3412e99134edbfe44bd51ab784d6204e9cac69ed
walle <myeongwoo95@naver.com>
2023-04-12 16:01:15 +0900

    :>

--- src/main/java/io/vitasoft/dvorakbackend/controller/dto/user/UserSignInRequestDto.java
+++ src/main/java/io/vitasoft/dvorakbackend/controller/dto/user/UserSignInRequestDto.java
@@ -1,15 +1,12 @@
 package io.vitasoft.dvorakbackend.controller.dto.user;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
-@Setter
 @NoArgsConstructor
 public class UserSignInRequestDto {
 
